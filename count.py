3a93700742a0b46f76e71e271bd9daf75a71a866
Maria Chiara <mariachiara.giorgi1@gmail.com>
2023-06-30 11:06:16 +0200

    d

--- count.py
+++ count.py
@@ -1,57 +1,54 @@
-from geopy.geocoders import Nominatim
-import folium
-import pandas as pd
-from geopy.exc import GeocoderTimedOut
-from functools import lru_cache
+import csv
 
-# Carica i dati dal file CSV
-data = pd.read_csv('MAmapcount.csv')
+input_file = 'partyspettro.csv'  # Nome del file CSV di input
+output_file = 'output.csv'  # Nome del file CSV di output
 
-# Crea una mappa di base centrata sull'Italia utilizzando Folium
-italy_map = folium.Map(location=[41.8719, 12.5674], zoom_start=6)
+# Dizionario per tenere traccia del conteggio di uomini e donne per partito
+parties = {}
 
-# Funzione per ottenere le coordinate della regione utilizzando Geocoder
-@lru_cache(maxsize=100)
-def get_coordinates(region):
-    geolocator = Nominatim(user_agent='mariachiara.giorgi1@gmail.com')
-    try:
-        location = geolocator.geocode(region + ', Italy')
-        if location is not None:
-            return location.latitude, location.longitude
-    except GeocoderTimedOut:
-        return get_coordinates(region)
+# Apri il file di input in modalità lettura
+with open(input_file, 'r') as file:
+    reader = csv.reader(file)
+    header = next(reader)  # Leggi l'intestazione del file
 
-    return None
+    # Indici delle colonne nel file di input
+    party_index = header.index('partito')
+    gender_index = header.index('gender')
+    alignment_index = header.index('Allineamento Politico')
 
-# Itera sui dati del DataFrame e crea layer per le regioni sulla mappa
-for index, row in data.iterrows():
-    region = row['regione']
-    count = row['count']
-    coordinates = get_coordinates(region)
+    # Elabora le righe del file di input
+    for row in reader:
+        party = row[party_index]
+        gender = row[gender_index]
+        alignment = row[alignment_index]
 
-    if coordinates:
-        folium.CircleMarker(
-            location=coordinates,
-            radius=count/1000,
-            color='blue',
-            fill=True,
-            fill_color='blue',
-            fill_opacity=0.6,
-            popup=f"{region}: {count}",
-        ).add_to(italy_map)
+        # Controlla se il partito esiste già nel dizionario
+        if party in parties:
+            if gender == 'Male':
+                parties[party]['male'] += 1
+            elif gender == 'Female':
+                parties[party]['female'] += 1
+        else:
+            # Crea un nuovo dizionario per il partito
+            parties[party] = {'male': 0, 'female': 0}
 
-# Aggiungi il layer Choropleth alla mappa utilizzando il file GeoJSON delle regioni italiane
-folium.Choropleth(
-    geo_data='italy_regions.geojson',  # Inserisci il percorso al file GeoJSON delle regioni italiane
-    name='choropleth',
-    data=data,
-    columns=['regione', 'count'],
-    key_on='feature.properties.name',
-    fill_color='YlOrRd',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Count'
-).add_to(italy_map)
+            if gender == 'Male':
+                parties[party]['male'] = 1
+            elif gender == 'Female':
+                parties[party]['female'] = 1
 
-# Visualizza la mappa
-italy_map.save('mappa.html')
+        # Aggiungi l'allineamento politico al dizionario del partito
+        parties[party]['alignment'] = alignment
+
+# Apri il file di output in modalità scrittura
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    # Scrivi l'intestazione del file di output
+    writer.writerow(['partito', 'male', 'female', 'allineamento politico'])
+
+    # Scrivi i dati per ogni partito nel file di output
+    for party, data in parties.items():
+        writer.writerow([party, data['male'], data['female'], data['alignment']])
+
+print("Elaborazione completata. Il nuovo file CSV è stato creato.")
