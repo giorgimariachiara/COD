196a39e945b6bd0cd09a0104f9377e5c8651947b
Maria Chiara <mariachiara.giorgi1@gmail.com>
2023-07-20 11:41:48 +0200

    c

--- ministrixleg.py
+++ ministrixleg.py
@@ -1,3 +1,5 @@
+"""
+
 import pandas as pd
 
 # Leggi il file CSV con i dati
@@ -8,3 +10,19 @@ df = df.drop(['nome', 'cognome'], axis=1)
 
 # Salva il dataframe in un nuovo file CSV
 df.to_csv('ministriFGL.csv', index=False)
+"""
+
+import pandas as pd
+
+# Carica il file CSV originale
+file_input = "ministrixleF.csv"
+dataframe = pd.read_csv(file_input)
+
+# Elimina la colonna "gender"
+dataframe = dataframe.drop(columns=["gender"])
+
+# Salva il nuovo dataframe in un nuovo file CSV chiamato "ministre.csv"
+file_output = "ministre.csv"
+dataframe.to_csv(file_output, index=False)
+
+print(f"File {file_output} creato con successo senza la colonna 'gender'.")
