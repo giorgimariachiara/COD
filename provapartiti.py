35d9eeeaba93360b8cb4a0af58bd69e91c2f432d
elizastuglik <elizastuglik6798@gmail.com>
2023-10-10 10:15:59 +0200

    codice partiti

--- provapartiti.py
+++ provapartiti.py
@@ -368,7 +368,5 @@ df_completo_alignment.loc[df_completo_alignment['partito'] == 'DEMOCRAZIA NAZION
 df_completo_alignment.loc[df_completo_alignment['partito'] == 'PSI-PSDI UNIFICATI', 'Allineamento Politico'] = 'centro-sinistra'
 df_completo_alignment.loc[df_completo_alignment['Allineamento Politico'] == 'sinistra radicale', 'Allineamento Politico'] = 'estrema sinistra'
 
-dovesiete = df_completo_alignment[df_completo_alignment['Allineamento Politico'].isna()]
-print(dovesiete)
-#df_completo_alignment.to_csv('partyallineamento.csv', index=False)
+df_completo_alignment.to_csv('partyallineamento.csv', index=False)
 
