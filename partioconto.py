5f37fd70c92dab6015cbcc4f407db7e098ea86f2
Maria Chiara <mariachiara.giorgi1@gmail.com>
2023-10-10 10:25:11 +0200

    k

--- partioconto.py
+++ partioconto.py
@@ -21,6 +21,7 @@ grouped = grouped.rename(columns={'male': 'numeroUomini', 'female': 'numeroDonne
 grouped.to_csv('conteggio_generi_per_partito.csv', index=False)
 """
 
+"""
 import pandas as pd
 
 # Carica il file CSV in un DataFrame
@@ -33,4 +34,25 @@ df['PercentualeDonne'] = ((df['numeroDonne'] / (df['numeroDonne'] + df['numeroUo
 df['PercentualeUomini'] = ((df['numeroUomini'] / (df['numeroDonne'] + df['numeroUomini'])) * 100).round().astype(int)
 
 # Salva il DataFrame aggiornato in un nuovo file CSV
-df.to_csv('percentuali_generi_per_partito.csv', index=False)
+df.to_csv('percentagePartiti.csv', index=False)
+"""
+
+
+import pandas as pd
+
+# Carica il file CSV in un DataFrame
+df = pd.read_csv('percentagePartiti.csv.csv')
+
+# Raggruppa il DataFrame per 'AllineamentoPolitico' e calcola la media delle percentuali di donne
+media_percentuali_donne = df.groupby('AllineamentoPolitico')['PercentualeDonne'].mean()
+
+# Trova l'allineamento politico con la percentuale di donne più alta
+allineamento_con_più_donne = media_percentuali_donne.idxmax()
+percentuale_più_donne = media_percentuali_donne.max()
+
+# Trova l'allineamento politico con la percentuale di donne più bassa
+allineamento_con_meno_donne = media_percentuali_donne.idxmin()
+percentuale_meno_donne = media_percentuali_donne.min()
+
+print(f"Allineamento politico con più donne: {allineamento_con_più_donne} ({percentuale_più_donne:.2f}%)")
+print(f"Allineamento politico con meno donne: {allineamento_con_meno_donne} ({percentuale_meno_donne:.2f}%)")
